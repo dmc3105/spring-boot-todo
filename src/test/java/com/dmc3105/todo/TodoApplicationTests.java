98908ca069e01051ff0218a23993e576b9b34cbd
Говоров А. В <algovor04@gmail.com>
2024-06-09 03:23:09 +0700

    Init commit

---
+++ src/test/java/com/dmc3105/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmc3105.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
